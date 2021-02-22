4ba993d914b98c3126b24692d5e8033f427e7daa
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-02-22 13:51:31 +0100

    Repaso básico HTML

--- HTML5/01/scripts/script.js
+++ HTML5/01/scripts/script.js
@@ -1,3 +1,3 @@
 console.log("Mi primer javascript");
 
-setTimeout(function() { alert("Prueba de TimeOut || Segundo JavaScript "); }, 3000);
+//setTimeout(function() { alert("Prueba de TimeOut || Segundo JavaScript "); }, 3000);
