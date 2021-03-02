e5b4199ee39a47fd7d599799df6ee9341da5d55a
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-03-02 12:15:43 +0100

    Inicio JavaScript

--- JS/01- Hola Mundo/scripts/main.js
+++ JS/01- Hola Mundo/scripts/main.js
@@ -1,1 +1,17 @@
 console.log("Hola Mundo");
+
+// Variables //
+let numero = 1;
+console.log(numero);
+
+let booleano = true;
+console.log(booleano);
+
+let nulo = null;
+console.log(nulo);
+
+let noDefinido = undefined;
+console.log(noDefinido);
+
+let texto = 'texto prueba';
+console.log(texto);
