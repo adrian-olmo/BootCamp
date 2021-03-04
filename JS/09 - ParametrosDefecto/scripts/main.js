98c2aaa91c8fe132d8f726fd952e5263a5140e66
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-03-04 21:36:08 +0100

    Inicio JavaScript

--- JS/09 - ParametrosDefecto/scripts/main.js
+++ JS/09 - ParametrosDefecto/scripts/main.js
@@ -2,12 +2,19 @@ let nombre = prompt('Introduce tu nombre');
 let letrasNom = nombre.substring(0, 2).toUpperCase();
 let letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+let num1, num2;
+let regla = (num1, num2, letrasNom) => letrasNom + num1 + num2;
+let i = 0;
 
-let posicionLetra;
-let regla = (posicionLetra, letrasNom) => letrasNom + posicionLetra;
-
-for (let i = 0; i >= letras.length; i++) {
-
+while ((!num1 || !num2) && i < letras.length) {
+    if (letrasNom[0] == letras[i]) {
+        num1 = i + 1;
+    }
+    if (letrasNom[1] == letras[i]) {
+        num2 = i + 1;
+    }
+    i++;
 }
 
-console.log(regla(posicionLetra, letrasNom));
+
+console.log(regla(num1, num2, letrasNom));
